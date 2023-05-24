0c85090dcbc0b831ad6dd04a555bee7fc1032718
Levonz <106668322+Levonz@users.noreply.github.com>
2023-05-24 20:46:50 +0700

    Tính Đa Hình

--- 24_05_Buoi_12/Class3.py
+++ 24_05_Buoi_12/Class3.py
@@ -24,21 +24,27 @@ class PTTC1(BKCAD,Person):
         self.name = name
         self.age = age
     def giaoluu(self):
-        print('Giao Lưu')
+        print('Giao Lưu PTTC1')
 class PTTC2(BKCAD):
     def __init__(self,name,age):
         self.name = name
         self.age = age
+    def giaoluu(self):
+        print('Giao Lưu PTTC2')   
+        
 class PTTC1_offline(PTTC1):        
     def __init__(self,name,age):
         self.name = name
         self.age = age
-        
+  
+#Tính Đa hình
+def da_hinh(student):
+    student.giaoluu()      
+    
 student1 = PTTC1('Quang',22)
-student1.hoc()
-student1.an()
 
 student2 = PTTC1_offline('Hoàng',23)
-student2.giaoluu()
-student2.hoc()
-student2.an()
+
+student3 = PTTC2('Tiến',32)
+da_hinh(student1)
+da_hinh(student3)
