c318cab1af0852c914c34159011e82dc67a789d6
Levonz <106668322+Levonz@users.noreply.github.com>
2023-05-24 19:29:20 +0700

    Buoi 12 7:29

---
+++ 24_05_Buoi_12/Class.py
@@ -0,0 +1,25 @@
+class PTTC1():
+    #Phương thức
+    name = 'abc'
+    age = 0
+    country = 'HN'
+    #Thuộc tính 
+    def hoc():
+        print('Phải học Python')
+    def thi():
+        print('Phải thi Python')
+     
+student1 = PTTC1()
+student1.name = 'Nguyen Van A'
+student1.age = '20'
+student1.country = 'Ha Noi'
+
+student2 = PTTC1()
+student2.name = 'Nguyen Van B'
+student2.age = '21'
+student2.country = 'Ha Noi'
+
+student3 = PTTC1()
+student3.name = 'Nguyen Van C'
+student3.age = '20'
+student3.country = 'Bac Ninh'
